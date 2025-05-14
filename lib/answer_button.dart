4bd2ce2dc6ddb2dc06193e2b3b4db46a044f56a1
Saimur Rahman <saimur.cdk@gmail.com>
2025-05-15 02:56:01 +0600

    Added a demo question

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,13 +1,25 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
-  const AnswerButton({super.key});
+
+  final String answerText;
+  final Function() onTap;
+
+  const AnswerButton({
+    required this.answerText,
+    required this.onTap,
+    super.key});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-        onPressed: (){},
-        child: const Text("Answer 1"));
+      
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color.fromARGB(255, 33, 1, 95),
+          foregroundColor: Colors.white,
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 40)
+        ),
+        onPressed: onTap,
+        child: Text(answerText));
   }
 }
