f514e4c7998ecf31c1a758d1578c8225bea9ddc5
Saimur Rahman <saimur.cdk@gmail.com>
2025-05-17 01:59:53 +0600

    Fixed Ran Out Questions Error In Second Iteration

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -26,6 +26,7 @@ class _QuizState extends State<Quiz> {
     selectedAnswers.add(answer);
 
     if (selectedAnswers.length == questions.length){
+      selectedAnswers = [];
       setState(() {
         activeScreen = 'Start-Screen';
       });
